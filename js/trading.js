34e6c88aecab620cd066ff37573d163bc07fcb2a
Swizec Teller <swizec@swizec.com>
2011-03-05 15:52:11 +0100

    realized the newsletter function was replicated a lot

--- js/trading.js
+++ js/trading.js
@@ -88,20 +88,3 @@ function draw_market() {
 	});
     });
 };
-
-
-$('#newsletter').submit(function() {
-    requestData = { email: $('[name=email]').val() };			
-    $.ajax({
-	type: 'POST',
-	url: 'list/subscribe', 
-	contentType: 'application/json; charset=utf-8', 
-	dataType: 'json',
-	processData : false,
-	data: JSON.stringify(requestData),
-	success: function(data) {
-	    console.log(data);
-	}
-    });
-    return false;
-});
